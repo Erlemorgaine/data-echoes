1771dcfc2a80fa22eaa2381f16a381f359263ef0
Erlemorgaine <erle_3@hotmail.com>
2023-11-14 23:22:36 +0100

    new buikd

--- data-echoes/vite.config.ts
+++ data-echoes/vite.config.ts
@@ -5,7 +5,7 @@ import vue from '@vitejs/plugin-vue'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: '/data-echoes',
+  base: '/data-echoes/',
   plugins: [vue()],
   resolve: {
     alias: {
