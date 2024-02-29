71fa2c670d62f38d2c17e99d45d532ebc960ac23
Erlemorgaine <erle_3@hotmail.com>
2024-02-29 10:38:41 +0100

    add a whole bunch of data, organise presidents royals page better, add threejs instances code

--- data-echoes/src/components/presidents-royals/PresidentsRoyalsPage.vue
+++ data-echoes/src/components/presidents-royals/PresidentsRoyalsPage.vue
@@ -10,6 +10,9 @@ import {
   Mesh,
   Vector3,
   BufferGeometry,
+  InstancedBufferGeometry,
+  InstancedBufferAttribute,
+  BufferAttribute,
   LineBasicMaterial,
   Line,
 } from 'three'
@@ -31,6 +34,8 @@ import { nodes, links } from './data/network-data.json'
 const canvasContainer = ref<HTMLElement | null>(null)
 const nodeObject = ref({})
 
+const linksLength = links.length
+
 let renderer: WebGLRenderer | null
 let scene: Scene | null
 let controls: OrbitControls | null
@@ -38,6 +43,7 @@ let camera: PerspectiveCamera | null
 
 let nodePlanes: Mesh[] = []
 let linkLines: Line[] = []
+let linksGeometry: InstancedBufferGeometry | null
 
 onMounted(() => {
   nodeObject.value = nodes.reduce((prev, next) => {
@@ -47,58 +53,113 @@ onMounted(() => {
   createScene()
 })
 
-function createScene() {
+function addRenderer(width: number, height: number) {
   if (canvasContainer.value) {
-    const { width, height } = canvasContainer.value.getBoundingClientRect()
-
     const canvas = document.createElement('canvas')
     canvasContainer.value.appendChild(canvas)
 
-    scene = new Scene()
-
     renderer = new WebGLRenderer({ canvas })
     renderer.setSize(width, height)
+  }
+}
 
-    camera = new PerspectiveCamera(75, width / height, 0.1, 100)
-    camera.position.set(0, 0, 5)
-    scene.add(camera)
+function addCamera(scene: Scene, width: number, height: number) {
+  camera = new PerspectiveCamera(75, width / height, 0.1, 100)
+  camera.position.set(0, 0, 5)
+  scene.add(camera)
+}
 
-    controls = new OrbitControls(camera, renderer.domElement)
-    controls.enableDamping = true
-    controls.enableZoom = true
+function addControls(camera: PerspectiveCamera, renderer: WebGLRenderer) {
+  controls = new OrbitControls(camera, renderer.domElement)
+  controls.enableDamping = true
+  controls.enableZoom = true
 
-    controls.update()
+  controls.update()
+}
 
-    const planeGeometry = new PlaneGeometry(0.5, 0.5)
-    const planeMaterial = new MeshBasicMaterial({ color: 0xffff00, side: DoubleSide })
+function createNodePlanes(scene: Scene) {
+  const planeGeometry = new PlaneGeometry(0.5, 0.5)
+  const planeMaterial = new MeshBasicMaterial({ color: 0xffff00, side: DoubleSide })
 
-    // Create planes for each node in the nodes array
-    nodePlanes = nodes.map(() => {
-      const material = planeMaterial.clone()
-      material.color.setRGB(Math.random(), Math.random(), Math.random())
+  // Create planes for each node in the nodes array
+  nodePlanes = nodes.map(() => {
+    const material = planeMaterial.clone()
+    material.color.setRGB(Math.random(), Math.random(), Math.random())
 
-      const plane = new Mesh(planeGeometry, material)
-      plane.position.set(Math.random() * 2, Math.random() * 2, Math.random() * 2)
-      scene.add(plane)
+    const plane = new Mesh(planeGeometry, material)
+    plane.position.set(Math.random() * 2, Math.random() * 2, Math.random() * 2)
+    scene.add(plane)
 
-      return plane
-    })
+    return plane
+  })
+}
 
-    // Create lines for each link in the links array
-    const points: Vector3[] = []
-    points.push(new Vector3(0, 0, 0))
-    points.push(new Vector3(0, 0, -10))
+function createLinkLines(scene: Scene) {
+  //   // Create base geometry (line of length 1 along X-axis)
+  //   const baseGeometry = new BufferGeometry()
+  //   const vertices = new Float32Array([-0.5, 0.5, 0, 0.5, 0.5, 0])
+  //   baseGeometry.setAttribute('position', new BufferAttribute(vertices, 3))
 
-    const lineMaterial = new LineBasicMaterial({ color: 0xffffff })
+  //   // Create instanced geometry
+  //   const instancedGeometry = new InstancedBufferGeometry()
+  //   instancedGeometry.index = baseGeometry.index
+  //   instancedGeometry.attributes = baseGeometry.attributes
 
-    // TODO: Try to reuse the geometry somehow or at least make it more performant
-    linkLines = links.map(() => {
-      const lineGeometry = new BufferGeometry().setFromPoints(points)
-      const line = new Line(lineGeometry, lineMaterial)
-      scene.add(line)
+  //   // Set up instanced attributes (length and position) for each link
 
-      return line
-    })
+  //   const instanceLengths = new Float32Array(linksLength)
+  //   const instancePositions = new Float32Array(linksLength * 3)
+
+  //   // Set instance properties (length and position)
+  //   for (let i = 0; i < linksLength; i++) {
+  //     instanceLengths[i] = Math.random() * 2
+  //     instancePositions[i * 3] = Math.random() * 10 - 5 // X
+  //     instancePositions[i * 3 + 1] = Math.random() * 10 - 5 // Y
+  //     instancePositions[i * 3 + 2] = Math.random() * 10 - 5 // Z
+  //   }
+
+  //   instancedGeometry.setAttribute('length', new InstancedBufferAttribute(instanceLengths, 1))
+
+  //   instancedGeometry.setAttribute(
+  //     'positionOffset',
+  //     new InstancedBufferAttribute(instancePositions, 3),
+  //   )
+
+  //   const lineMaterial = new LineBasicMaterial({ color: 0xffffff })
+  //   const lines = new Line(instancedGeometry, lineMaterial)
+  //   scene.add(lines)
+  //   linksGeometry = instancedGeometry
+
+  // Create lines for each link in the links array
+  const points: Vector3[] = []
+  points.push(new Vector3(0, 0, 0))
+  points.push(new Vector3(0, 0, -10))
+
+  const lineMaterial = new LineBasicMaterial({ color: 0xffffff })
+
+  // TODO: Try to reuse the geometry somehow or at least make it more performant
+  linkLines = links.map(() => {
+    const lineGeometry = new BufferGeometry().setFromPoints(points)
+    const line = new Line(lineGeometry, lineMaterial)
+    scene.add(line)
+
+    return line
+  })
+}
+
+function createScene() {
+  if (canvasContainer.value) {
+    // Setup for scene
+    const { width, height } = canvasContainer.value.getBoundingClientRect()
+    scene = new Scene()
+
+    addRenderer(width, height)
+    addCamera(scene, width, height)
+    addControls(camera, renderer)
+
+    // Objects in scene
+    createNodePlanes(scene)
+    createLinkLines(scene)
 
     animate()
     forceNetwork()
@@ -134,6 +195,16 @@ function forceNetwork() {
         line.geometry.setFromPoints(updatedVertices)
       }
     })
+
+    // // Update instance properties (length and position)
+    // for (let i = 0; i < linksLength; i++) {
+    //   linksGeometry.attributes.length.setX(i, Math.random() * 2 + 1) // Random length between 1 and 3
+    //   linksGeometry.attributes.positionOffset.setXYZ(i, source.x, source.y, 0)
+    // }
+
+    // // Notify Three.js that attributes have changed
+    // linksGeometry.attributes.length.needsUpdate = true
+    // linksGeometry.attributes.positionOffset.needsUpdate = true
 
     nodes.forEach((node) => {
       nodePlanes[node.index].position.set(node.x, node.y, node.z || 0)
